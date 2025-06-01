lStewieAl/Kerfstok

commit 5b3418fa8a56285dd538f040b70c6891cedcbb7c

Fix type match errors

source/calc.mc
@@ -156,11 +156,11 @@ function tokenize(chars, len) {
   var oper = 1;
   var neg = 1;
   for (var i = 0; i < len; i++) {
-    var el;
+    var el = chars[0];
     for (; i < len; i++) {
       el = chars[i];
       if (el >= '0' && el <= '9') {
-        get = get * 10 + (el.toNumber() - 48);
+        get = get * 10 + (el.toNumber() - '0');
       } else {
         if (el == '.') {
           if (point != nopoint) {

source/AskDelegate.mc
@@ -9,7 +9,6 @@ class AskDelegate extends WatchUi.BehaviorDelegate {
   }
 
   function onBack() {
-    onResponse(false);
     WatchUi.popView(WatchUi.SLIDE_IMMEDIATE);
     return true;
   }
@@ -18,9 +17,7 @@ class AskDelegate extends WatchUi.BehaviorDelegate {
     if (clickEvent.getType() == CLICK_TYPE_TAP) {
       var co = clickEvent.getCoordinates();
       var y = co[1];
-      var x = co[0];
       if (y * 2 > height) {
-        onResponse(x * 2 > width);
         WatchUi.popView(WatchUi.SLIDE_IMMEDIATE);
       }
     }

source/AskView.mc
@@ -7,10 +7,11 @@ using Toybox.Time.Gregorian;
 class AskView extends WatchUi.View {
   var comments;
   var question;
-  function initialize(c, q) {
+  
+  function initialize(comments, questions) {
     View.initialize();
-    comments = c;
-    question = q;
+    self.comments = comments;
+    self.question = questions;
   }
 
   function onLayout(dc) {}
@@ -45,14 +46,14 @@ class AskView extends WatchUi.View {
       wmid,
       height * 0.2,
       Gfx.FONT_LARGE,
-      comments,
+      self.comments,
       Gfx.TEXT_JUSTIFY_VCENTER | Gfx.TEXT_JUSTIFY_CENTER
     );
     dc.drawText(
       wmid,
       height * 0.5,
       Gfx.FONT_LARGE,
-      question,
+      self.question,
       Gfx.TEXT_JUSTIFY_VCENTER | Gfx.TEXT_JUSTIFY_CENTER
     );
     dc.drawText(

source/Cancel.mc
@@ -7,6 +7,8 @@ class CancelConfirmationDelegate extends WatchUi.ConfirmationDelegate {
   function onResponse(response) {
     if (response == WatchUi.CONFIRM_YES) {
       WatchUi.popView(WatchUi.SLIDE_IMMEDIATE);
+      return true;
     }
+    return false;
   }
 }

source/Delete.mc
@@ -12,6 +12,8 @@ class DeleteConfirmationDelegate extends WatchUi.ConfirmationDelegate {
   function onResponse(response) {
     if (response == WatchUi.CONFIRM_YES) {
       delete(base, datanum);
+      return true;
     }
+    return false;
   }
 }

source/DialDelegate.mc
@@ -20,7 +20,7 @@ class numStore {
     return "";
   }
 
-  function toshoweffect() {
+  function toshoweffect() as Void {
     var tim = Time.now();
     var prec = precvars[varid];
     var rounded = round(floval, prec);

source/EmptyView.mc
@@ -38,7 +38,6 @@ class EmptyView extends WatchUi.View {
       dc.getFontHeight(clockfont) /
       (edge1040 || edge840 ? 1.8 : edge830 ? 2.1 : 2.7);
     theight = dc.getFontHeight(Gfx.FONT_NUMBER_HOT);
-    return true;
   }
 
   function onUpdate(dc) {

source/FarAway.mc
@@ -16,6 +16,8 @@ class FarWayConfirmationDelegate extends WatchUi.ConfirmationDelegate {
 
       havenums(base);
       WatchUi.popView(WatchUi.SLIDE_IMMEDIATE);
+      return true;
     }
+    return false;
   }
 }

commit 95e165bd3e02bb3f9b32d0f2b323ec1f936c9fa9

Fix type error when deleting numbers in input field (regression in 5b3418fa)

source/calc.mc
@@ -160,7 +160,7 @@ function tokenize(chars, len) {
     for (; i < len; i++) {
       el = chars[i];
       if (el >= '0' && el <= '9') {
-        get = get * 10 + (el.toNumber() - '0');
+        get = get * 10 + (el.toNumber() - 48); // '0'
       } else {
         if (el == '.') {
           if (point != nopoint) {